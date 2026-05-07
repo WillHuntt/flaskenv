e12339e128c90edd9539713e6454f2deec068935
Will <willhuntnz2003@gmail.com>
2026-05-07 14:45:02 +1200

    Product Page Work

--- products.py
+++ products.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/products")
 def products():
-    return render_template("index.html")
+    return render_template("products.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
