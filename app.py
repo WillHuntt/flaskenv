7eebef9fcb31b9439fb8cb3909b7f16d9ccb157a
Will <willhuntnz2003@gmail.com>
2026-05-08 12:25:27 +1200

    cleaned up product category sidebar by making each link use flexbox.

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def products():
+def index():
     return render_template("index.html")
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- products.py
+++ products.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/products")
+@app.route("/")
 def products():
     return render_template("products.html")
 
